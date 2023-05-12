e283c965c11f037762798d9ca95e9c73031708a8
baihaqilp <baihaqiluhurpangestika@gmail.com>
2023-05-12 22:14:10 +0700

    first initialize

---
+++ src/test/java/id/co/metrodata/serverApp/ServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.metrodata.serverApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
