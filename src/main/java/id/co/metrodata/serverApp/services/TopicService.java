f7290ac8f93e006a862b2f9558f2f898623c8e9f
naufal27 <mnaufalr27@gmail.com>
2023-05-18 03:42:27 +0700

    feat topic: add relation program

--- src/main/java/id/co/metrodata/serverApp/services/TopicService.java
+++ src/main/java/id/co/metrodata/serverApp/services/TopicService.java
@@ -1,8 +1,10 @@
 package id.co.metrodata.serverApp.services;
 
 import id.co.metrodata.serverApp.models.Topic;
+import id.co.metrodata.serverApp.models.dto.request.TopicRequest;
 import id.co.metrodata.serverApp.repositories.TopicRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,6 +15,8 @@ import java.util.List;
 @AllArgsConstructor
 public class TopicService {
     private TopicRepository topicRepository;
+    private ModelMapper modelMapper;
+    private ProgramService programService;
 
     public List<Topic> getAll() {
         return topicRepository.findAll();
@@ -24,12 +28,16 @@ public class TopicService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Topic not Found!"));
     }
 
-    public Topic create(Topic topic) {
+    public Topic create(TopicRequest topicRequest) {
+        Topic topic = modelMapper.map(topicRequest, Topic.class);
+        topic.setProgram(programService.getById(topicRequest.getProgram_id()));
         return topicRepository.save(topic);
     }
 
-    public Topic update(Topic topic, Long id) {
+    public Topic update(TopicRequest topicRequest, Long id) {
         getById(id);
+        Topic topic = modelMapper.map(topicRequest, Topic.class);
+        topic.setProgram(programService.getById(topicRequest.getProgram_id()));
         topic.setId(id);
         return topicRepository.save(topic);
     }
