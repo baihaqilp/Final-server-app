5e451674185fcbd9c7398ab2396e8e6995524429
dhimas <dhimas.alvianrifita@gmail.com>
2023-05-20 22:53:55 +0700

    Refactor Task - Deadline LocalDateTime

--- src/main/java/id/co/metrodata/serverApp/models/dto/request/TaskRequest.java
+++ src/main/java/id/co/metrodata/serverApp/models/dto/request/TaskRequest.java
@@ -1,14 +1,11 @@
 package id.co.metrodata.serverApp.models.dto.request;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 import lombok.Data;
 
@@ -16,8 +13,8 @@ import lombok.Data;
 public class TaskRequest {
     private String name;
     private String desc;
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm", iso = ISO.DATE_TIME)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm", iso = ISO.DATE_TIME)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy'T'HH:mm")
     private LocalDateTime deadline;
     private Long segmentId;
 }
